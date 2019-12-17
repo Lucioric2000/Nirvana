e3dbdcc4ff8f22cfd9c05c4fb430f3e20de3f5fb
Lucio Montero <lucioric2000@hotmail.com>
2019-12-17 07:12:26 +0100

    Installer script now works

--- install_Nirvana.bash
+++ install_Nirvana.bash
@@ -1,26 +1,4 @@
 #!/bin/bash
-#qseqdnamatch=`expr match "$(pwd)" '.*\(Nirvana\)'`
-#if [[ $qseqdnamatch = "Nirvana" ]]
-#then
-#    echo "Already in Nirvana folder."
-#    git pull origin master
-#else
-#    #Now, this script, when executed from outside the qiaseq folder, it downloads the qiaseq repository and then executes the script 'install_qiaseq_dna.bash'.
-#    #This allows that the installer be updated and not to have to provide the updated installer script
-#    echo "Not in Nirvana folder."
-#    if [[ -d "Nirvana" ]]
-#    then
-#        cd Nirvana && ./install_Nirvana.bash $@
-#    elif [[ -e "Nirvana" ]]
-#    the
-#        echo "File Nirvana exists but it is not a directory, thus we can not create a directory with that path tho hold the software reposotory. \
-#        See if it is safe to delete or move it, and then execute again this script."
-#    else
-#        git clone https://github.com/Lucioric2000/Nirvana
-#        cd Nirvana && ./install_Nirvana.bash $@
-#    fi
-#    exit
-#fi
 
 # adjust these paths to reflect where you have downloaded the Nirvana data files
 # In this example, we assume that the Cache, References, and SupplementaryDatabase
@@ -34,17 +12,15 @@
 
 sudo rpm -Uvh https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm
 sudo yum update
-sudo yum remove aspnetcore-runtime-2.1 dotnet-sdk-2.1
-sudo yum install aspnetcore-runtime-2.2 dotnet-sdk-2.2
-#sudo yum install aspnetcore-runtime-2.0 #Use the version used to build the package
+sudo yum install aspnetcore-runtime-2.1 dotnet-sdk-2.1
 
 # Nirvana install
 
-TOP_DIR=/usr/local
-NIRVANA_ROOT=/usr/local/Nirvana
-NIRVANA_BIN=$NIRVANA_ROOT/bin/Release/netcoreapp2.0/Nirvana.dll
+TOP_DIR=~
+NIRVANA_ROOT=$TOP_DIR/Nirvana
+NIRVANA_BIN=$NIRVANA_ROOT/bin/Release/netcoreapp2.1/Nirvana.dll
 DATA_DIR=$NIRVANA_ROOT/Data
-DOWNLOADER_BIN=$NIRVANA_ROOT/bin/Release/netcoreapp2.0/Downloader.dll
+DOWNLOADER_BIN=$NIRVANA_ROOT/bin/Release/netcoreapp2.1/Downloader.dll
 NIRVANA_TAG=v3.5.0
 
 # just change this to GRCh38 if you want to set everything up for hg38
@@ -113,40 +89,6 @@ download() {
     fi
 }
 
-#sudo_create_dir $NIRVANA_ROOT
-#sudo_create_dir $CACHE_DIR
-#sudo_create_dir $REF_DIR
-#sudo_create_dir $SA_DIR
-
-
-# ==============================
-# download all of the data files
-# ==============================
-
-#URL_LIST=""
-#
-#if [ ! -f $REF_TEST ] && [ ! -f $REF_TGZ ]
-#then
-#	URL_LIST="$URL_LIST http://illumina-annotation.s3.amazonaws.com/References/v${REF_VERSION}.tar.gz"
-#fi
-
-#if [ ! -f $CACHE_TEST ] && [ ! -f $CACHE_TGZ ]
-#then
-#    URL_LIST="$URL_LIST http://illumina-annotation.s3.amazonaws.com/Cache/v${CACHE_VERSION}.tar.gz"
-#fi
-
-#if [ ! -f $SA_TEST ] && [ ! -f $SA_TGZ ]
-#then
-#    URL_LIST="$URL_LIST http://illumina-annotation.s3.amazonaws.com/SA/$SA_VERSION/v${SA_VERSION}_${GENOME_ASSEMBLY}.tar.gz"
-#fi
-
-#cd $NIRVANA_ROOT
-#if [ ! -z "$URL_LIST" ]
-#then
-#    echo -n "- downloading up to ~27 GB of data files in parallel (this will probably take a while)... "
-#    echo $URL_LIST | xargs -n 1 -P 8 wget -qcP Data
-#    echo "finished."
-#fi
 
 # =====================
 # unpack the data files
@@ -155,18 +97,14 @@ download() {
 unpack_file() {
     if [ ! -f $4 ]
     then
-	pushd $2 > /dev/null
-	echo -n "- unpacking $1 files... ($2) ($3) ($4)"
-	sudo tar -xfz $3
-	echo "finished."
-	popd > /dev/null
-	rm $3
+    pushd $2 > /dev/null
+    echo -n "- unpacking $1 files... ($2) ($3) ($4)"
+    sudo tar -xfz $3
+    echo "finished."
+    popd > /dev/null
+    rm $3
     fi
 }
-
-#unpack_file "reference" $DATA_DIR $REF_TGZ $REF_TEST
-#unpack_file "cache" $DATA_DIR $CACHE_TGZ $CACHE_TEST
-#unpack_file "supplementary annotation $GENOME_ASSEMBLY" $SA_DIR $SA_TGZ $SA_TEST
 
 # ==================================
 # clone the Nirvana repo (if needed)
@@ -176,40 +114,30 @@ pushd $TOP_DIR
 
 if [ ! -d $NIRVANA_ROOT ]
 then
-    sudo git clone https://github.com/Lucioric2000/Nirvana.git
-    #sudo git checkout $NIRVANA_TAG
-    sudo git checkout merger
+    git clone https://github.com/Illumina/Nirvana.git
+    git checkout $NIRVANA_TAG
 fi
 
-# download the Nirvana source
-#if [ ! -f $SOURCE_TEST ]
-#then
-#    # need to dance a little bit since git will complain this is a non-empty directory
-#    git init
-#    git remote add origin https://github.com/Lucioric2000/Nirvana.git
-#    git fetch
-#    git checkout $NIRVANA_TAG
-#fi
 
 # =============
 # build Nirvana
 # =============
 
 pushd $NIRVANA_ROOT
-echo fi $NIRVANA_BIN
 if [ ! -f $NIRVANA_BIN ]
 then
     #pushd Nirvana > /dev/null
-    sudo dotnet build -c Release
+    dotnet clean
+    dotnet build -c Release -v diagnostic
     #popd > /dev/null
 fi
 
 # download the Nirvana source
 if [ ! -f $REF_TEST ]
 then
-    sudo_create_dir $DATA_DIR
+    create_dir $DATA_DIR
     echo dotnet $DOWNLOADER_BIN --ga $GENOME_ASSEMBLY --out $DATA_DIR
-    sudo dotnet $DOWNLOADER_BIN --ga $GENOME_ASSEMBLY --out $DATA_DIR
+    dotnet $DOWNLOADER_BIN --ga $GENOME_ASSEMBLY --out $DATA_DIR
 fi
 
 
@@ -220,11 +148,11 @@ fi
 # download a test vcf file
 if [ ! -f HiSeq.10000.vcf ]
 then
-    sudo wget https://github.com/samtools/htsjdk/raw/master/src/test/resources/htsjdk/variant/HiSeq.10000.vcf
+    wget https://github.com/samtools/htsjdk/raw/master/src/test/resources/htsjdk/variant/HiSeq.10000.vcf
 fi
 
 # analyze it with Nirvana
-COMMAND="dotnet $NIRVANA_BIN -c $CACHE_DIR/Ensembl --sd $SA_DIR/$GENOME_ASSEMBLY -r $REF_TEST -i HiSeq.10000.vcf -o HiSeq.10000.annotated"
+COMMAND="dotnet $NIRVANA_BIN -c $CACHE_DIR/Both --sd $SA_DIR -r $REF_TEST -i HiSeq.10000.vcf -o HiSeq.10000.annotated"
 echo Running $COMMAND
 $COMMAND
 
